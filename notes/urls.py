5aa2290914014b03893221c9d6232bb5ef9098a6
prs2rnn <developer.prs2rnn@gmail.com>
2026-05-20 00:13:34 +0500

    feat: crud

--- notes/urls.py
+++ notes/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.notes_list, name='notes_list'),
     path('<int:note_id>/', views.note_detail, name='note_detail'),
+    path('create/', views.note_create, name='note_create'),
+    path('<int:note_id>/edit/', views.note_edit, name='note_edit'),
+    path('<int:note_id>/delete/', views.note_delete, name='note_delete'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
+from django.views.decorators.http import require_http_methods
 
+from .forms import NoteForm
 from .models import Note
 
 
@@ -13,3 +15,42 @@ def note_detail(request, note_id):
     note = get_object_or_404(Note, pk=note_id)
 
     return render(request, 'notes/detail.html', {'note': note})
+
+
+def note_create(request):
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('notes_list')
+    else:
+        form = NoteForm()
+    return render(
+        request, 'notes/form.html', {'form': form, 'title': 'Создать заметку'}
+    )
+
+
+def note_edit(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    if request.method == 'POST':
+        form = NoteForm(request.POST, instance=note)
+        if form.is_valid():
+            form.save()
+            return redirect('note_detail', note_id=note.id)
+    else:
+        form = NoteForm(instance=note)
+
+    return render(
+        request, 'notes/form.html', {'form': form, 'title': 'Редактировать заметку'}
+    )
+
+
+@require_http_methods(['GET', 'POST'])
+def note_delete(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+
+    if request.method == 'POST':
+        note.delete()
+        return redirect('notes_list')
+
+    return render(request, 'notes/confirm_delete.html', {'note': note})
