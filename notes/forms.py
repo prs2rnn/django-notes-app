b9f7482e5ae118cb2951d19407a4e2368c6d212b
prs2rnn <developer.prs2rnn@gmail.com>
2026-05-31 16:21:47 +0500

    fix: update css and bootstrap version

--- notes/forms.py
+++ notes/forms.py
@@ -7,7 +7,19 @@ class NoteForm(forms.ModelForm):
     class Meta:
         model = Note
         fields = ['title', 'body']
+        labels = {'title': 'Note title', 'body': 'Note body'}
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control'}),
-            'body': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
+            'title': forms.TextInput(
+                attrs={
+                    'class': 'form-control',
+                    'placeholder': 'Enter the note title...',
+                }
+            ),
+            'body': forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    'rows': 5,
+                    'placeholder': 'Enter the text of the note...',
+                }
+            ),
         }
