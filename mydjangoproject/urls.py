8838f6fe80eb11acba383ae65f730d032a8f7b97
prs2rnn <developer.prs2rnn@gmail.com>
2026-05-31 14:04:36 +0500

    fix: add redirect to notes

--- mydjangoproject/urls.py
+++ mydjangoproject/urls.py
@@ -17,5 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
-urlpatterns = [path('admin/', admin.site.urls), path('notes/', include('notes.urls'))]
+urlpatterns = [
+    path('', RedirectView.as_view(url='notes/')),
+    path('admin/', admin.site.urls),
+    path('notes/', include('notes.urls')),
+]
