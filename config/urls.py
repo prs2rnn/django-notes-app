008d1319384beebbe09b463121a5625ece8c9f83
prs2rnn <developer.prs2rnn@gmail.com>
2026-06-07 11:31:48 +0500

    chore: admin panel url

--- config/urls.py
+++ config/urls.py
@@ -22,7 +22,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path("", include("core.urls")),
-    path("admin/", admin.site.urls),
+    path("super-secret-admin-panel/", admin.site.urls),
     path("notes/", include("notes.urls")),
     path("accounts/", include("accounts.urls")),
 ]
